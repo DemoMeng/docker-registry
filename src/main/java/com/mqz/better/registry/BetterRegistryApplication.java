427d933c45ec288f9e270ff0680bce0419943de2
蒙大拿 <977466992@qq.com>
2022-06-09 16:02:12 +0800

    优化

--- src/main/java/com/mqz/better/registry/BetterRegistryApplication.java
+++ src/main/java/com/mqz/better/registry/BetterRegistryApplication.java
@@ -1,12 +1,14 @@
 package com.mqz.better.registry;
 
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, MybatisAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.mqz.better.registry.**.**"})
 public class BetterRegistryApplication {
 
